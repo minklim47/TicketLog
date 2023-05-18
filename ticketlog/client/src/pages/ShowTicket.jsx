c5d860fb56667cdf82dee208061b07480e4d09eb
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-05-18 22:56:17 +0700

    display

--- ticketlog/client/src/pages/ShowTicket.jsx
+++ ticketlog/client/src/pages/ShowTicket.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import Ticket from "../components/Ticket";
-import { Box, IconButton } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
 function ShowTicket() {
@@ -41,7 +41,7 @@ function ShowTicket() {
       className="flex-container"
       sx={{ flexDirection: "column", marginTop: "100px" }}
     >
-         <Box
+      <Box
         className="flex-container"
         sx={{
           marginBottom: "30px",
@@ -49,30 +49,65 @@ function ShowTicket() {
           maxWidth: "600px",
           minWidth: "300px",
           padding: "0 30px",
-          
+          flexDirection: "column",
         }}
       >
-      <IconButton sx={{alignSelf:"flex-start"}}
-        onClick={() => {
-          navigate(`/Home/${userId}`);
-        }}
-      >
-        <ArrowBackIcon />
-      </IconButton>
-      {ticket.length === 0 ? (
-        <p></p>
-      ) : (
-        <Ticket
-          key={ticket[0].id}
-          ticket={ticket[0]}
+        <IconButton
+          sx={{ alignSelf: "flex-start" }}
           onClick={() => {
-            handleClick(ticket[0].id);
+            navigate(`/Home/${userId}`);
           }}
-        />
-      )}
+        >
+          <ArrowBackIcon />
+        </IconButton>
+        {ticket.length === 0 ? (
+          <p></p>
+        ) : (
+          <Ticket
+            key={ticket[0].id}
+            ticket={ticket[0]}
+            onClick={() => {
+              handleClick(ticket[0].id);
+            }}
+          />
+        )}
       </Box>
+      <Button sx={saveButtonStyle} type="submit">
+        Edit ticket
+      </Button>
+      <Button
+        sx={cancelButtonStyle}
+        variant="outlined"
+        color="black"
+
+      >
+        Delete ticket
+      </Button>
     </Box>
   );
 }
 
+const saveButtonStyle = {
+  textTransform: "none",
+  fontSize: 14,
+  width: "120px",
+  height: "30px",
+  bgcolor: "black.main",
+  color: "white.main",
+  "&:hover": {
+    backgroundColor: "black.dark",
+  },
+};
+const cancelButtonStyle = {
+  textTransform: "none",
+  fontSize: 14,
+  width: "120px",
+  height: "30px",
+  borderColor: "black.main",
+  color: "black.main",
+  margin: "30px 0",
+  "&:hover": {
+    backgroundColor: "black.light",
+  },
+};
 export default ShowTicket;
